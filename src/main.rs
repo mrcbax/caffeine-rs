003df34e5bd6aa538a6a3e0a597b0fb8d6a95d90
Chad Baxter <cbaxter@mail.umw.edu>
2017-02-22 03:03:56 -0500

    added early configuration support (toml)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 // Inivisible backgrounding for Windows based systems.
 #![feature(windows_subsystem)]
+#![feature(type_ascription)]
 #![windows_subsystem = "windows"]
+#![deny(warnings)]
 
-extern crate keystroke;
 extern crate chrono;
+extern crate keystroke;
+extern crate toml;
 
+#[macro_use]
+extern crate serde_derive;
+
+use std::fs::File;
+use std::io::prelude::*;
 use std::{thread, time};
 
+use chrono::Duration;
 use keystroke::{Key, Physical};
 use keystroke::{release_key};
-use chrono::Duration;
 
 fn main() {
-    keep_awake(59);
+    keep_awake(read_config().refresh_rate.unwrap());
+}
+
+
+/// This is what we're going to decode into. Each field is optional, meaning
+/// that it doesn't have to be present in TOML.
+#[derive(Debug, Deserialize)]
+struct Config {
+    refresh_rate: Option<i64>,
 }
 
 ///Resets the timer duration to a given value.
@@ -48,4 +64,17 @@ fn keep_awake(seconds: i64){
         thread::sleep(time::Duration::from_millis(1000));
         counter = counter - Duration::seconds(1);
     }
+}
+
+fn read_config() -> Config {
+    let mut input = String::new();
+    let name = String::from("caffeine.toml");
+    File::open(&name).and_then(|mut f| {
+        f.read_to_string(&mut input)
+    }).unwrap();
+
+    let some_duration: Option<i64> = Some(59);
+    let mut decoded: Config = Config { refresh_rate: some_duration };
+    decoded: Config = toml::from_str(input.as_str()).unwrap();
+    decoded
 }

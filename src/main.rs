197d97018058f40cda45fb0b8132574e924cbce3
Chad Baxter <cbaxter@mail.umw.edu>
2017-02-22 02:00:40 -0500

    added documentation commentation.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+/// Inivisible backgrounding for Windows based systems.
 #![feature(windows_subsystem)]
 #![windows_subsystem = "windows"]
 
@@ -14,16 +15,28 @@ fn main() {
     keep_awake(59);
 }
 
+///Resets the timer duration to a given value.
+///
+/// # Arguments
+///
+/// * `counter` - A `chrono` crate `Duration`.
+/// * `seconds` - An i64 for the number of seconds to add to the duration.
 fn reset_timer(counter: Duration, seconds: i64) -> Duration {
     counter + Duration::seconds(seconds)
 }
 
+///Sends a key-release action to the OS.
 fn send_key_press() {
     use Key::Physical;
     use Physical::Shift;
     release_key(Physical(Shift));
 }
 
+///Loop manager.
+///
+/// #Arguments
+///
+/// * `seconds` - The number of seconds to pass to `reset-timer`.
 fn keep_awake(seconds: i64){
     let sintinel: bool = true;
     let mut counter = reset_timer(Duration::seconds(seconds), 0);

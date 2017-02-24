1e19ca01072f33220d0a56127008866ffe2af360
Chad Baxter <cbaxter@mail.umw.edu>
2017-02-24 16:50:52 -0500

    added more functionality

--- src/main.rs
+++ src/main.rs
@@ -22,10 +22,18 @@ use chrono::Duration;
 #[derive(Debug, Deserialize)]
 struct Config {
     refresh_rate: Option<i64>,
+    start_after: Option<i64>,
+    exit_after: Option<i64>,
+    active_for: Option<i64>,
 }
 
 fn main() {
-    keep_awake(read_config().refresh_rate.unwrap());
+    let config: Config = read_config();
+    let refresh_rate: i64 = config.refresh_rate.unwrap();
+    let start_after: i64 = config.start_after.unwrap();
+    let exit_after: i64 = config.exit_after.unwrap();
+    let active_for: i64 = config.active_for.unwrap();
+    keep_awake(refresh_rate, start_after, exit_after, active_for);
 }
 
 ///Resets the timer duration to a given value.
@@ -42,19 +50,35 @@ fn reset_timer(counter: Duration, seconds: i64) -> Duration {
 ///
 /// #Arguments
 ///
-/// * `seconds` - The number of seconds to pass to `reset-timer`.
-fn keep_awake(seconds: i64){
-    let sintinel: bool = true;
-    let mut counter = reset_timer(Duration::seconds(seconds), 0);
+fn keep_awake(refresh_rate: i64, start_after: i64, exit_after: i64, active_for: i64){
+    thread::sleep(time::Duration::new(start_after as u64, 0));
+    let mut exit: i64 = 0;
+    let mut active: i64 = 0;
+    if exit_after > 0 {
+        exit = exit_after;
+    }
+    if active_for > 0 {
+        active = active_for;
+    }
+    let mut sintinel: bool = true;
+    let mut counter = reset_timer(Duration::seconds(refresh_rate), 0);
     while sintinel {
         if counter == Duration::seconds(0) {
-            counter = reset_timer(counter, seconds);
+            counter = reset_timer(counter, refresh_rate);
             platform::platform::send_key_press();
             println!("sent keys.");
         }
         thread::sleep(time::Duration::from_millis(1000));
         println!("{} seconds remaining", counter);
         counter = counter - Duration::seconds(1);
+        exit -= 1;
+        active -= 1;
+        if exit <= 0 || active <= 0 {
+            sintinel = false;
+        }
+    }
+    if active >= 0 {
+        thread::sleep(time::Duration::new(active as u64, 0));
     }
 }
 
@@ -64,8 +88,7 @@ fn keep_awake(seconds: i64){
 /// Passes along the configuration data as a struct, returns defaults if no config is specified.
 fn read_config() -> Config {
     let name = String::from("caffeine.toml");
-    let some_duration: Option<i64> = Some(59);
-    let mut decoded: Config = Config { refresh_rate: some_duration };
+    let mut decoded: Config = Config { refresh_rate: Some(59), start_after: Some(0), exit_after: Some(-1), active_for: Some(-1)};
     if path_exists(&name) {
         let mut input = String::new();
         fs::File::open(&name).and_then(|mut f| {
